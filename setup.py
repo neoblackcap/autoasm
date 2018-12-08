2500c9d078b382996d67be4fb61f007b484cfa1d
Neo Ko <neo.blackcap@gmail.com>
2018-12-08 15:26:33 +0800

    Enhancement: setup ci

--- setup.py
+++ setup.py
@@ -2,14 +2,8 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name='conveyor',
-    version='0.1',
     packages=find_packages(),
-    url='https://github.com/neoblackcap/conveyor',
-    license='Apache License 2.0',
-    author='neo',
-    author_email='neo.blackcap@gmail.com',
-    description='conveyor',
+    setup_requires=["pytest-runner"],
     install_requires=[],
     tests_require=['pytest==4.0.1', 'pytest-mock==1.10.0']
 )
