76eca299e446fde0e6a565827795f9e96b94d826
Neo Ko <neo.blackcap@gmail.com>
2018-12-09 20:52:09 +0800

    Enhancement: add coverall.io support

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     setup_requires=["pytest-runner"],
     install_requires=[],
     tests_require=['pytest==4.0.1', 'pytest-mock==1.10.0', 'coverage>=4.5.2',
-                   'pytest-cov==2.6.0', 'coveralls']
+                   'pytest-cov==2.6.0']
 )
